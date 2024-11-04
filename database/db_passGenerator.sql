d9b933d0aa90b4b92f8e1a1b655b9d382e6b4e18
samumuku <sallakusamuel39@gmail.com>
2024-11-04 15:01:48 +0100

    modifs

--- database/db_passGenerator.sql
+++ database/db_passGenerator.sql
@@ -7,7 +7,6 @@ USE db_passGenerator;
 
 CREATE TABLE t_password (
     idPassword INT AUTO_INCREMENT,
-    username VARCHAR(100),
     savedPassword VARCHAR(50),
     PRIMARY KEY (idPassword)
 );
--- save_password.php
+++ save_password.php
@@ -1,27 +1,25 @@
 <?php
 
-// Include database connection
 $host = 'localhost';        // Database server (use '127.0.0.1' or 'localhost')
-$dbname = 'db_passgenerator'; // Database name
-$user = 'root';      // Database username
-$password = 'root';  // Database password
+$dbName = 'db_passgenerator'; // Database name
+$mySqlUser = 'root';      // Database username
+$mySqlPassword = 'root';  // Database password
 
 // Create connection
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $mySqlUser, $mySqlPassword, $dbName);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 
 // Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form data
-    $username = $conn->real_escape_string($_POST['username']);
     $savedPassword = $conn->real_escape_string($_POST['savedPassword']);
     
     $savedPassword = password_hash($savedPassword, PASSWORD_DEFAULT);
-
 
     // Prepare and execute SQL statement
     $sql = "INSERT INTO t_password (savedPassword) VALUES ('$savedPassword')";
